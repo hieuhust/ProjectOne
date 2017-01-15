6f1a8f2338c1c7450b7eb3ed6f5c3f1e20e9f792
Hoang Kien An <kienandtvt@gmail.com>
2017-01-15 11:36:10 +0700

    update map activity

--- app/src/main/java/com/anonymous/carchecker/position/view/MapInfoActivity.java
+++ app/src/main/java/com/anonymous/carchecker/position/view/MapInfoActivity.java
@@ -1,15 +1,203 @@
 package com.anonymous.carchecker.position.view;
 
+import android.graphics.Point;
+import android.location.Location;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.anonymous.carchecker.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 
-public class MapInfoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCallback {
+    int currentPt;
+    // Keep track of our markers
+    private List<Marker> markers = new ArrayList<Marker>();
+
+    private GoogleMap googleMap;
+
+    private Marker selectedMarker;
+
+    GoogleMap.CancelableCallback simpleAnimationCancelableCallback =
+            new GoogleMap.CancelableCallback(){
+
+                @Override
+                public void onCancel() {
+                }
+
+                @Override
+                public void onFinish() {
+
+                    if(++currentPt < markers.size()){
+
+//					double heading = SphericalUtil.computeHeading(googleMap.getCameraPosition().target, markers.get(currentPt).getPosition());
+//					System.out.println("Heading  = " + (float)heading);
+//					float targetBearing = bearingBetweenLatLngs(googleMap.getCameraPosition().target, markers.get(currentPt).getPosition());
+//					System.out.println("Bearing  = " + targetBearing);
+//
+                        LatLng targetLatLng = markers.get(currentPt).getPosition();
+
+                        CameraPosition cameraPosition =
+                                new CameraPosition.Builder()
+                                        .target(targetLatLng)
+                                        .tilt(currentPt<markers.size()-1 ? 90 : 0)
+                                        //.bearing((float)heading)
+                                        .zoom(googleMap.getCameraPosition().zoom)
+                                        .build();
+
+
+                        googleMap.animateCamera(
+                                CameraUpdateFactory.newCameraPosition(cameraPosition),
+                                3000,
+                                simpleAnimationCancelableCallback);
+
+                        highLightMarker(currentPt);
+
+                    }
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_info);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+                addMarkerToMap(latLng);
+            }
+
+        });
+
+        addDefaultLocations();
+        startAnimation();
+    }
+
+    private void startAnimation() {
+        resetMarkers();
+        googleMap.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(markers.get(0).getPosition(), 16),
+                5000,
+                simpleAnimationCancelableCallback);
+
+        currentPt = 0-1;
+    }
+    private void addDefaultLocations() {
+        clearMarkers();
+        addMarkerToMap(new LatLng(50.961813797827055,3.5168474167585373));
+        addMarkerToMap(new LatLng(50.96085423274633,3.517405651509762));
+        addMarkerToMap(new LatLng(50.96020550146382,3.5177918896079063));
+        addMarkerToMap(new LatLng(50.95936754348453,3.518972061574459));
+        addMarkerToMap(new LatLng(50.95877285446026,3.5199161991477013));
+        addMarkerToMap(new LatLng(50.958179213755905,3.520646095275879));
+        addMarkerToMap(new LatLng(50.95901719316589,3.5222768783569336));
+        addMarkerToMap(new LatLng(50.95954430150347,3.523542881011963));
+        addMarkerToMap(new LatLng(50.95873336312275,3.5244011878967285));
+        addMarkerToMap(new LatLng(50.95955781702322,3.525688648223877));
+        addMarkerToMap(new LatLng(50.958855004782116,3.5269761085510254));
+    }
+
+    private Location convertLatLngToLocation(LatLng latLng) {
+        Location loc = new Location("someLoc");
+        loc.setLatitude(latLng.latitude);
+        loc.setLongitude(latLng.longitude);
+        return loc;
+    }
+
+    private float bearingBetweenLatLngs(LatLng begin,LatLng end) {
+        Location beginL= convertLatLngToLocation(begin);
+        Location endL= convertLatLngToLocation(end);
+        return beginL.bearingTo(endL);
+    }
+
+    public void toggleStyle() {
+        if (GoogleMap.MAP_TYPE_NORMAL == googleMap.getMapType()) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+
+    /**
+     * Adds a marker to the map.
+     */
+    public void addMarkerToMap(LatLng latLng) {
+        Marker marker = googleMap.addMarker(new MarkerOptions().position(latLng)
+                .title("Kien An")
+                .snippet("Map example"));
+        markers.add(marker);
+
+    }
+
+    /**
+     * Clears all markers from the map.
+     */
+    public void clearMarkers() {
+        googleMap.clear();
+        markers.clear();
+    }
+
+    /**
+     * Remove the currently selected marker.
+     */
+    public void removeSelectedMarker() {
+        this.markers.remove(this.selectedMarker);
+        this.selectedMarker.remove();
+    }
+
+    /**
+     * Highlight the marker by index.
+     */
+    private void highLightMarker(int index) {
+        highLightMarker(markers.get(index));
+    }
+
+    /**
+     * Highlight the marker by marker.
+     */
+    private void highLightMarker(Marker marker) {
+        marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        marker.showInfoWindow();
+        //Utils.bounceMarker(googleMap, marker);
+        this.selectedMarker=marker;
+    }
+
+    /**
+     * Ensure that all markers are using the default red colored icon. (removes any highlighted icons).
+     */
+    private void resetMarkers() {
+        for (Marker marker : this.markers) {
+            marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        }
     }
 }
