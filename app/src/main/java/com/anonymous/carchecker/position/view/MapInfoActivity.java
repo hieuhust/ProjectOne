903a97912e3de965ea0d9b9856fc7c2ea4a75a08
Hoang Kien An <kienandtvt@gmail.com>
2017-01-15 16:39:48 +0700

    update map activity

--- app/src/main/java/com/anonymous/carchecker/position/view/MapInfoActivity.java
+++ app/src/main/java/com/anonymous/carchecker/position/view/MapInfoActivity.java
@@ -1,6 +1,5 @@
 package com.anonymous.carchecker.position.view;
 
-import android.graphics.Point;
 import android.location.Location;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -13,7 +12,6 @@ import com.anonymous.carchecker.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -21,19 +19,23 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCallback {
     int currentPt;
-    // Keep track of our markers
+
     private List<Marker> markers = new ArrayList<Marker>();
 
     private GoogleMap googleMap;
 
     private Marker selectedMarker;
+
+    private final Handler mHandler = new Handler();
+
+    private Animator animator = new Animator();
 
     GoogleMap.CancelableCallback simpleAnimationCancelableCallback =
             new GoogleMap.CancelableCallback(){
@@ -47,18 +49,12 @@ public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCall
 
                     if(++currentPt < markers.size()){
 
-//					double heading = SphericalUtil.computeHeading(googleMap.getCameraPosition().target, markers.get(currentPt).getPosition());
-//					System.out.println("Heading  = " + (float)heading);
-//					float targetBearing = bearingBetweenLatLngs(googleMap.getCameraPosition().target, markers.get(currentPt).getPosition());
-//					System.out.println("Bearing  = " + targetBearing);
-//
                         LatLng targetLatLng = markers.get(currentPt).getPosition();
 
                         CameraPosition cameraPosition =
                                 new CameraPosition.Builder()
                                         .target(targetLatLng)
                                         .tilt(currentPt<markers.size()-1 ? 90 : 0)
-                                        //.bearing((float)heading)
                                         .zoom(googleMap.getCameraPosition().zoom)
                                         .build();
 
@@ -87,25 +83,15 @@ public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-
-            @Override
-            public void onMapClick(LatLng latLng) {
-                addMarkerToMap(latLng);
-            }
-
-        });
 
         addDefaultLocations();
-        startAnimation();
+        animator.startAnimation(true);
     }
 
     private void startAnimation() {
         resetMarkers();
         googleMap.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(markers.get(0).getPosition(), 16),
+                CameraUpdateFactory.newLatLngZoom(markers.get(0).getPosition(), 20),
                 5000,
                 simpleAnimationCancelableCallback);
 
@@ -113,6 +99,8 @@ public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCall
     }
     private void addDefaultLocations() {
         clearMarkers();
+//        addMarkerToMap(new LatLng(21.734106,105.25956));
+//        addMarkerToMap(new LatLng(21.697653,105.438698));
         addMarkerToMap(new LatLng(50.961813797827055,3.5168474167585373));
         addMarkerToMap(new LatLng(50.96085423274633,3.517405651509762));
         addMarkerToMap(new LatLng(50.96020550146382,3.5177918896079063));
@@ -148,9 +136,6 @@ public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCall
     }
 
 
-    /**
-     * Adds a marker to the map.
-     */
     public void addMarkerToMap(LatLng latLng) {
         Marker marker = googleMap.addMarker(new MarkerOptions().position(latLng)
                 .title("Kien An")
@@ -159,32 +144,20 @@ public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCall
 
     }
 
-    /**
-     * Clears all markers from the map.
-     */
     public void clearMarkers() {
         googleMap.clear();
         markers.clear();
     }
 
-    /**
-     * Remove the currently selected marker.
-     */
     public void removeSelectedMarker() {
         this.markers.remove(this.selectedMarker);
         this.selectedMarker.remove();
     }
 
-    /**
-     * Highlight the marker by index.
-     */
     private void highLightMarker(int index) {
         highLightMarker(markers.get(index));
     }
 
-    /**
-     * Highlight the marker by marker.
-     */
     private void highLightMarker(Marker marker) {
         marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
         marker.showInfoWindow();
@@ -192,12 +165,242 @@ public class MapInfoActivity extends AppCompatActivity implements OnMapReadyCall
         this.selectedMarker=marker;
     }
 
-    /**
-     * Ensure that all markers are using the default red colored icon. (removes any highlighted icons).
-     */
     private void resetMarkers() {
         for (Marker marker : this.markers) {
             marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
         }
     }
+
+    public class Animator implements Runnable {
+
+        private static final int ANIMATE_SPEEED = 1500;
+        private static final int ANIMATE_SPEEED_TURN = 1000;
+        private static final int BEARING_OFFSET = 20;
+
+        private final Interpolator interpolator = new LinearInterpolator();
+
+        int currentIndex = 0;
+
+        float tilt = 90;
+        float zoom = 15.5f;
+        boolean upward=true;
+
+        long start = SystemClock.uptimeMillis();
+
+        LatLng endLatLng = null;
+        LatLng beginLatLng = null;
+
+        boolean showPolyline = false;
+
+        private Marker trackingMarker;
+
+        public void reset() {
+            resetMarkers();
+            start = SystemClock.uptimeMillis();
+            currentIndex = 0;
+            endLatLng = getEndLatLng();
+            beginLatLng = getBeginLatLng();
+
+        }
+
+        public void stop() {
+            trackingMarker.remove();
+            mHandler.removeCallbacks(animator);
+
+        }
+
+        public void initialize(boolean showPolyLine) {
+            reset();
+            this.showPolyline = showPolyLine;
+
+            highLightMarker(0);
+
+            if (showPolyLine) {
+                polyLine = initializePolyLine();
+            }
+
+            // We first need to put the camera in the correct position for the first run (we need 2 markers for this).....
+            LatLng markerPos = markers.get(0).getPosition();
+            LatLng secondPos = markers.get(1).getPosition();
+
+            setupCameraPositionForMovement(markerPos, secondPos);
+
+        }
+
+        private void setupCameraPositionForMovement(LatLng markerPos,
+                                                    LatLng secondPos) {
+
+            float bearing = bearingBetweenLatLngs(markerPos,secondPos);
+
+            trackingMarker = googleMap.addMarker(new MarkerOptions().position(markerPos)
+                    .title("title")
+                    .snippet("snippet"));
+
+            CameraPosition cameraPosition =
+                    new CameraPosition.Builder()
+                            .target(markerPos)
+                            .bearing(bearing + BEARING_OFFSET)
+                            .tilt(90)
+                            .zoom(googleMap.getCameraPosition().zoom >=16 ? googleMap.getCameraPosition().zoom : 16)
+                            .build();
+
+            googleMap.animateCamera(
+                    CameraUpdateFactory.newCameraPosition(cameraPosition),
+                    ANIMATE_SPEEED_TURN,
+                    new GoogleMap.CancelableCallback() {
+
+                        @Override
+                        public void onFinish() {
+                            System.out.println("finished camera");
+                            animator.reset();
+                            Handler handler = new Handler();
+                            handler.post(animator);
+                        }
+
+                        @Override
+                        public void onCancel() {
+                            System.out.println("cancelling camera");
+                        }
+                    }
+            );
+        }
+
+        private Polyline polyLine;
+        private PolylineOptions rectOptions = new PolylineOptions();
+
+
+        private Polyline initializePolyLine() {
+            //polyLinePoints = new ArrayList<LatLng>();
+            rectOptions.add(markers.get(0).getPosition());
+            return googleMap.addPolyline(rectOptions);
+        }
+
+        /**
+         * Add the marker to the polyline.
+         */
+        private void updatePolyLine(LatLng latLng) {
+            List<LatLng> points = polyLine.getPoints();
+            points.add(latLng);
+            polyLine.setPoints(points);
+        }
+
+
+        public void stopAnimation() {
+            animator.stop();
+        }
+
+        public void startAnimation(boolean showPolyLine) {
+            if (markers.size()>=2) {
+                animator.initialize(showPolyLine);
+            }
+        }
+
+
+        @Override
+        public void run() {
+
+            long elapsed = SystemClock.uptimeMillis() - start;
+            double t = interpolator.getInterpolation((float)elapsed/ANIMATE_SPEEED);
+
+//			LatLng endLatLng = getEndLatLng();
+//			LatLng beginLatLng = getBeginLatLng();
+
+            double lat = t * endLatLng.latitude + (1-t) * beginLatLng.latitude;
+            double lng = t * endLatLng.longitude + (1-t) * beginLatLng.longitude;
+            LatLng newPosition = new LatLng(lat, lng);
+
+            trackingMarker.setPosition(newPosition);
+
+            if (showPolyline) {
+                updatePolyLine(newPosition);
+            }
+
+            // It's not possible to move the marker + center it through a cameraposition update while another camerapostioning was already happening.
+            //navigateToPoint(newPosition,tilt,bearing,currentZoom,false);
+            //navigateToPoint(newPosition,false);
+
+            if (t< 1) {
+                mHandler.postDelayed(this, 16);
+            } else {
+
+                System.out.println("Move to next marker.... current = " + currentIndex + " and size = " + markers.size());
+                // imagine 5 elements -  0|1|2|3|4 currentindex must be smaller than 4
+                if (currentIndex<markers.size()-2) {
+
+                    currentIndex++;
+
+                    endLatLng = getEndLatLng();
+                    beginLatLng = getBeginLatLng();
+
+
+                    start = SystemClock.uptimeMillis();
+
+                    LatLng begin = getBeginLatLng();
+                    LatLng end = getEndLatLng();
+
+                    float bearingL = bearingBetweenLatLngs(begin, end);
+
+                    highLightMarker(currentIndex);
+
+                    CameraPosition cameraPosition =
+                            new CameraPosition.Builder()
+                                    .target(end) // changed this...
+                                    .bearing(bearingL  + BEARING_OFFSET)
+                                    .tilt(tilt)
+                                    .zoom(googleMap.getCameraPosition().zoom)
+                                    .build();
+
+
+                    googleMap.animateCamera(
+                            CameraUpdateFactory.newCameraPosition(cameraPosition),
+                            ANIMATE_SPEEED_TURN,
+                            null
+                    );
+
+                    start = SystemClock.uptimeMillis();
+                    mHandler.postDelayed(animator, 16);
+
+                } else {
+                    currentIndex++;
+                    highLightMarker(currentIndex);
+                    stopAnimation();
+                }
+
+            }
+        }
+
+
+
+
+        private LatLng getEndLatLng() {
+            return markers.get(currentIndex+1).getPosition();
+        }
+
+        private LatLng getBeginLatLng() {
+            return markers.get(currentIndex).getPosition();
+        }
+
+        private void adjustCameraPosition() {
+            //System.out.println("tilt = " + tilt);
+            //System.out.println("upward = " + upward);
+            //System.out.println("zoom = " + zoom);
+            if (upward) {
+
+                if (tilt<90) {
+                    tilt ++;
+                    zoom-=0.01f;
+                } else {
+                    upward=false;
+                }
+
+            } else {
+                if (tilt>0) {
+                    tilt --;
+                    zoom+=0.01f;
+                } else {
+                    upward=true;
+                }
+            }
+        }
+    };
 }
